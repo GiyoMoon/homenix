2cc841c2915d7d84d562c868c3a5bd641d3c1a00
Jasmin <code@jasi.dev>
2024-09-10 19:41:51 +0200

    feat: only one k3s server

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -1,10 +1,20 @@
-{ config, meta, ... }:
+{
+  lib,
+  config,
+  meta,
+  ...
+}:
+let
+  isMainNode = meta.hostname == "node1";
+  serverNodes = [ "node1" ];
+  role = if builtins.elem meta.hostname serverNodes then "server" else "agent";
+in
 {
   networking.firewall = {
     allowedTCPPorts = [
       6443 # K3s supervisor and Kubernetes API Server
-      2379 # Required only for HA with embedded etcd
-      2380 # Required only for HA with embedded etcd
+      # 2379 # Required only for HA with embedded etcd
+      # 2380 # Required only for HA with embedded etcd
     ];
     allowedUDPPorts = [
       8472 # Required only for Flannel VXLAN
@@ -13,14 +23,18 @@
 
   services.k3s = {
     enable = true;
-    role = "server";
+    inherit role;
     # Get token
     # cat /var/lib/rancher/k3s/server/token
     tokenFile = config.sops.secrets.k3s_token.path;
-    clusterInit = (meta.hostname == "node1");
-    serverAddr = if (meta.hostname != "node1") then "https://node1.lan:6443" else "";
-    extraFlags = [
-      ("--tls-san " + meta.hostname + ".lan")
-    ];
+    clusterInit = isMainNode;
+    serverAddr = if !isMainNode then "https://node1.lan:6443" else "";
+    extraFlags =
+      [
+        # "--cluster-reset" # If everything breaks, this will save you :3
+      ]
+      ++ lib.optionals (role == "server") [
+        ("--tls-san " + meta.hostname + ".lan")
+      ];
   };
 }
